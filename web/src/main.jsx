467d7c580249035f23c50e3247e2377a72a87799
JianB <79104723+BeerensJian@users.noreply.github.com>
2022-11-30 15:05:11 +0100

    SCSS variables, restructure

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './index.scss'
+import './style.scss'
 
 // view imports
 import AppMenu from './views/AppMenu/AppMenu'
@@ -23,7 +23,6 @@ const router = createBrowserRouter([
       },
     ]
   },
-  
 ])
 
 
