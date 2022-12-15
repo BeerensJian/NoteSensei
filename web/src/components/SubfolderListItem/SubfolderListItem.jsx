ebc41f2f1058b2782b365c9809efe5affde7c84d
JianB <79104723+BeerensJian@users.noreply.github.com>
2022-12-15 20:25:41 +0100

    Show subfolders of subfolder

--- web/src/components/SubfolderListItem/SubfolderListItem.jsx
+++ web/src/components/SubfolderListItem/SubfolderListItem.jsx
@@ -1,9 +1,18 @@
 import './SubfolderListItem.css'
+import { useState } from 'react'
+import SubfolderList from '../SubfolderList/SubfolderList'
+
 
 const SubfolderListItem = ({ folder }) => {
+  const [openFolder, setOpenFolder] = useState(false)
+
+
   return (
-    <div className='subfolder-listitem'>
-      <span>{folder.name}</span>
+    <div className="subfolder">
+      <div onClick={() => setOpenFolder(!openFolder)} className='subfolder-listitem'>
+        <span>{folder.name}</span>
+      </div>
+      {openFolder && <SubfolderList parent={folder._id}/>}
     </div>
   )
 }
